93fda125b1b6992fffe3f13c6c8f0edb22304606
Daniel <daniel.huseby26@d155.org>
2024-11-04 08:45:33 -0600

    added an option to delete the classes, and to reset the student's datas

---
+++ classInfoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  classInfoViewController.swift
+//  UsefulAppHuseby
+//
+//  Created by DANIEL HUSEBY on 11/4/24.
+//
+
+import UIKit
+
+class classInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
