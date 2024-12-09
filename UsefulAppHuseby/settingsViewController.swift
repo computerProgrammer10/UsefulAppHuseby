fdeadad5ba653c028d1cc9a49c0fb7f94f90ca0a
John <john.gariepy25@d155.org>
2024-12-09 08:45:06 -0600

    shoudl work but idk

--- UsefulAppHuseby/settingsViewController.swift
+++ UsefulAppHuseby/settingsViewController.swift
@@ -18,6 +18,7 @@ class settingsViewController: UIViewController {
     
     @IBOutlet weak var studentCountOutlet: UILabel!
     
+    @IBOutlet weak var editBackgroundColorOutlet: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -69,6 +70,22 @@ class settingsViewController: UIViewController {
         self.present(alert, animated: true)
     }
     
+    @IBAction func editBackgroundColor(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Edit Background", message: "Type a new name for the Background Color", preferredStyle: .alert)
+        alert.addTextField{ (textfield) in
+            textfield.placeholder = "Enter color Here!"
+        
+        let action = UIAlertAction(title: "Submit", style: .default) {(action) in
+            AppData.saves[AppData.curSlot].thisColor = alert.textFields![0].text!
+                AppData.saveData()
+                self.reloadData()
+                
+            }
+            let action2 = UIAlertAction(title: "Cancel", style: .cancel)
+            alert.addAction(action)
+            alert.addAction(action2)
+            self.present(alert, animated: true)
+    }
     /*
     // MARK: - Navigation
 
